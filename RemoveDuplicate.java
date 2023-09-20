1b01e1893ccdfdac418bf39388751dcf36409b56
akashpkakar27 <122001797+akashpkakar27@users.noreply.github.com>
2023-09-20 03:34:00 -0800

    Add files via upload

---
+++ RemoveDuplicate.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class RemoveDuplicate {
+
+	public static void main(String[] args) {
+		int a[] = {1,2,3,4,2,1,4};
+		System.out.println("Duplicate Elements");
+		
+		for(int i=0;i<a.length;i++) {
+			for(int j=i+1;j<a.length;j++) {
+				if (a[i]==a[j] ){
+				System.out.print(a[j]+ " ");	
+				} 
+			}
+		}
+	}
+
+}
